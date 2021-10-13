29182208da0a906297a0e0259c9305f79bda9f1f
Leonel Escobar <leo_8_11@hotmail.com>
2021-10-13 12:09:40 -0300

    Creación de la carpeta components y navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import './styles/NavBar.css'
+
+function NavBar () {
+    return (
+        <div className='nav'>
+            <div className='container'>
+                <div className='nav__container'>
+                    <div className='nav__title'>
+                        <h1>Nombre de la tienda</h1>
+                    </div>
+                    <div className='nav__menu'>
+                        <ul>
+                            <li>Inicio</li>
+                            <li>Sección 1</li>
+                            <li>Sección 2</li>
+                            <li>Sección 3</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
